3986daee3024be233e859a33e3e13bc51f315654
Valenhater <xnitezl@gmail.com>
2024-03-11 11:11:53 +0100

    Add project files.

---
+++ MvcCorePaginacionRegistros/Controllers/EmpleadosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCorePaginacionRegistros.Models;
+using MvcCorePaginacionRegistros.Repositories;
+
+namespace MvcCorePaginacionRegistros.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryHospital repo;
+
+        public EmpleadosController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> EmpleadosDepartamento()
+        {
+            List<Departamento> departamentos = await this.repo.GetDepartamentosAsync();
+            return View(departamentos);
+        }
+        public async Task<IActionResult> DepartamentoEmpleados(int iddepartamento)
+        {
+            List<Empleado> emp = await this.repo.GetEmpleadosDepartamentoAsync(iddepartamento);
+            return View(emp);
+
+        }
+    }
+}
---
+++ MvcCorePaginacionRegistros/Repositories/RepositoryHospital.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCorePaginacionRegistros.Data;
+using MvcCorePaginacionRegistros.Models;
+
+namespace MvcCorePaginacionRegistros.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            return await this.context.Departamentos.ToListAsync();
+        }
+
+        public async Task <List<Empleado>> GetEmpleadosDepartamentoAsync(int idDepartamento)
+        {
+            var empleados = this.context.Empleados.Where(x => x.IdDepartamento == idDepartamento);
+
+            if (empleados.Count() == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return await empleados.ToListAsync();
+            }
+        }
+    }
+}
